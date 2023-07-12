37a6583af4fb5b4139e7ca502645233a3463435c
alexisnsns <alexisns@pm.me>
2023-07-12 16:54:03 -0400

    django heroku

--- connectislice/settings.py
+++ connectislice/settings.py
@@ -127,5 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# import django_heroku
-# django_heroku.settings(locals())
+
+import django_heroku
+django_heroku.settings(locals())
