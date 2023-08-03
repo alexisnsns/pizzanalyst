ff82dcc8ecd56de0b18407a4009cb31449ae20e7
alexisnsns <alexisns@pm.me>
2023-08-03 16:33:16 -0400

    average grade

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,12 +1,19 @@
 from cloudinary.models import CloudinaryField
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models import Avg
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     address = models.CharField(max_length=100, blank=True)
     image = CloudinaryField('image', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def average_quality(self):
+        return self.comments.aggregate(avg_quality=Avg('quality'))['avg_quality']
 
 class Comment(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='comments')
