09045d77dd09f6ee19b7d636cc0d53495ec3cdbe
alexisnsns <alexisns@pm.me>
2023-08-11 16:03:39 -0400

    set up all authentication

--- restaurants/forms.py
+++ restaurants/forms.py
@@ -6,7 +6,7 @@ class RestaurantForm(forms.ModelForm):
     class Meta:
         model = Restaurant
         fields = ['name', 'description', 'address', 'image']
-
+        
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
---
+++ restaurants/static/mapbox.js
@@ -0,0 +1,6 @@
+import { autofill } from '@mapbox/search-js-web';
+
+
+autofill({
+  accessToken: 'pk.eyJ1IjoiYWxleGlzbnMiLCJhIjoiY2xoaG5zZmkxMGFpajNybDlrdTNseWZxNSJ9.3QS5KtBNtabxKxifVwSkDw'
+  })
