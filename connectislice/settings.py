4ddb8cdad7d9768e170f452eba1327eb9bb4a34a
alexisnsns <alexisns@pm.me>
2023-10-03 19:59:14 -0400

    fix key

--- connectislice/settings.py
+++ connectislice/settings.py
@@ -12,7 +12,7 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['connectislice-a773b07f800c.herokuapp.com', 'localhost', 'www.anotherproject.me']
 
