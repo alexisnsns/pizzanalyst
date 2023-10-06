e4938854e1805842df196c92e4d10664a0c3428e
alexisnsns <alexisns@pm.me>
2023-10-06 17:44:14 -0400

    fix settings

--- connectislice/settings.py
+++ connectislice/settings.py
@@ -10,9 +10,8 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['connectislice-a773b07f800c.herokuapp.com', 'localhost', 'www.anotherproject.me']
